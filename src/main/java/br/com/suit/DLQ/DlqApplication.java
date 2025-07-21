c70329a0497ea8e543c43739f6f2da3170a21a25
Diogo Valente <diogovalente@hotmail.com.br>
2025-07-21 19:51:30 -0300

    Implementa DQL

---
+++ src/main/java/br/com/suit/DLQ/DlqApplication.java
@@ -0,0 +1,13 @@
+package br.com.suit.DLQ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DlqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DlqApplication.class, args);
+	}
+
+}
